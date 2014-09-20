1d9a25b4ee25ee16b1021346cbd4165803a9671b
ArunKumar <arunmailer@gmail.com>
2014-09-21 00:42:33 +0530

    Using Content script

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-var Tlist = document.getElementById("stream-items-id").getElementsByTagName("li");
-for (var i = 0; i < Tlist.length; i++) {
-    
-    var pList = Tlist[i].getElementsByTagName("p");
-    for (var j = 0; j < pList.length; j++) {
-        var tweet = pList[j].textContent;
-        console.log(tweet);
-        var dList = Tlist[i].getElementsByTagName("div");
-        for (var k = 0; k < dList.length; k++) {
-            var tweetid = dList[k].getAttribute("data-tweet-id");
-            var tweetScreen = dList[k].getAttribute("data-screen-name");
-            console.log(tweetid);
-            console.log(tweetScreen);
-            break;
+var runscript = function() {
+    var Tlist = document.getElementById("stream-items-id").getElementsByTagName("li");
+    //var count = 0;
+    for (var i = 0; i < Tlist.length; i++) {
+        var pList = Tlist[i].getElementsByTagName("p");
+        for (var j = 0; j < pList.length; j++) {
+            var tweet = pList[j].textContent;
+            //console.log(tweet);
+            //count++;
+            var dList = Tlist[i].getElementsByTagName("div");
+            for (var k = 0; k < dList.length; k++) {
+                var tweetid = dList[k].getAttribute("data-tweet-id");
+                var tweetScreen = dList[k].getAttribute("data-screen-name");
+                //console.log(tweetid);
+                //console.log(tweetScreen);
+                break;
+            }
         }
-    }
- }
+     }
+     //console.log(count);
+     setTimeout(runscript, 30000);
+};
+
+ runscript();
+//document.body.addEventListener("DOMNodeInserted", runscript, false);
+//document.body.addEventListener("DOMNodeInsertedIntoDocument", runscript, true);
