b2839e24e12dc19221058b56f8349c29a6b8f489
ArunKumar <arunmailer@gmail.com>
2014-09-20 20:03:35 +0530

    Fixing the infinite loop

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var Tlist = document.getElementById("stream-items-id").getElementsByTagName("li");
 for (var i = 0; i < Tlist.length; i++) {
-	
+    
     var pList = Tlist[i].getElementsByTagName("p");
     for (var j = 0; j < pList.length; j++) {
-    	var tweet = pList[j].textContent;
-    	console.log(tweet);
-    	var dList = Tlist[i].getElementsByTagName("div");
-		for (var j = 0; j < dList.length; j++) {
-			var tweetid = dList[j].getAttribute("data-tweet-id");
-			var tweetScreen = dList[j].getAttribute("data-screen-name");
-			console.log(tweetid);
-			console.log(tweetScreen);
-			break;
-		}
+        var tweet = pList[j].textContent;
+        console.log(tweet);
+        var dList = Tlist[i].getElementsByTagName("div");
+        for (var k = 0; k < dList.length; k++) {
+            var tweetid = dList[k].getAttribute("data-tweet-id");
+            var tweetScreen = dList[k].getAttribute("data-screen-name");
+            console.log(tweetid);
+            console.log(tweetScreen);
+            break;
+        }
     }
  }
